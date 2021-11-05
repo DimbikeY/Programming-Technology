6851cc7549b2f36c5a351d8ff329c6d250879498
DimbikeY <mr.dolmatov-2001@mail.ru>
2021-11-05 17:34:39 +0300

    Not done!

---
+++ lab3/Loop/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("\nwhile: \t\t");
+            int i = 1;
+            while (i<=n)
+            {
+                Console.Write(" " + i);
+                i += 2;
+            }
+            
+            Console.Write("\ndo while: \t");
+            i = 1;
+            do
+            {
+                Console.Write(" " + i);
+                i += 2;
+            }
+            while (i <= n);  // do выполняет хотя бы 1 раз даже при нарушении условия.
+            
+            Console.Write("\nFor: \t\t");
+            for (i = 1; i <= n; i += 2)
+            {
+                Console.Write(" " + i);
+            }
+
+            // Используем цикл с постусловием.
+            double x, x1, x2, y;  // Объявляем 4 переменных вещественного типа.
+            Console.Write("\nWrite the left border of the interval = ");
+            x1 = double.Parse(Console.ReadLine());
+            Console.Write("Write the right border of the interval = ");
+            x2 = double.Parse(Console.ReadLine());
+            x = x1;
+            do
+            {
+                y = Math.Sin(x);
+                x += 0.01;
+            }
+            while (x <= x2);
+
+            // Используем цикл с предусловием.
+            int a, b, temp;
+            Console.Write("\nWrite a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Write b = ");
+            b = int.Parse(Console.ReadLine());
+            temp = a; // Дальше будет алгоритм Евклида.
+            while (temp != b) // Допустим, возьмем числа a = 3 b= 5; 3!=5
+            {
+                a = temp;  // temp = 3
+                if (a<b)   // 3 < 5 -> true
+                {
+                    temp = a; // temp = 3
+                    a = b;     // a = 5
+                    b = temp;  // b = 3
+                    temp = a - b; // 3 - 5 = -2
+                    a = b;        // a = 3 как и b = 3. Условие выполнилось (нарушилось)
+                }
+            }
+            // Реализуем задачу вывода значений функции с помощью цикла с предусловием
+            // Задача наоборот то есть.
+
+        }   
+    }
+}
---
+++ lab3/Task_1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write your favourite year = ");
+            int year = Int32.Parse(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
