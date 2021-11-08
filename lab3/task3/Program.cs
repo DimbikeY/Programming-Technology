2386da396940f91804a980372865b45e39bdfebc
DimbikeY <mr.dolmatov-2001@mail.ru>
2021-11-08 18:17:55 +0300

    Done!

--- lab3/task3/Program.cs
+++ lab3/task3/Program.cs
@@ -11,6 +11,14 @@ namespace task3
             string y;
             float sum;
             sum = 0f;
+            Random rnd = new Random(); // Создание объекта для генерации чисел
+            float x_value_center = rnd.Next(-10, 10) * 0.1f; // Получаем очередное (первое) случайное число в заданном диапазоне
+            // которое будет показывать случайное отклонение (в пределах нормального) центра мишени.
+            float y_value_center = rnd.Next(-10, 10) * 0.1f;
+            // Теперь введем попутный ветер :)
+            float wind_x_value = rnd.Next(-5, 5) * 0.2f;
+            float wind_y_value = rnd.Next(-5, 5) * 0.2f;
+
             Console.WriteLine("Hi. Welcome to the shooting range. Write your number on the list.");
             variant = int.Parse(Console.ReadLine()) % 2;
             Console.WriteLine("Write your shot point x. If you have finished shooting, then enter \"I have done\"");
@@ -19,14 +27,14 @@ namespace task3
             y = Console.ReadLine();
             while (x != "I have done" || y != "I have done")
             {
-                float[] history = new float[2];
+                float[] history = new float[2]; // Сохранение в массив
                 history[0] = float.Parse(x);
                 history[1] = float.Parse(y);
-                if ((history[0] <= 1.00f && history[0] >= -1.00f) && (history[1] >= -1.00f && history[1] <= 1.00f))
+                if ((history[0] + wind_x_value <= 1.00f + x_value_center && history[0] + wind_x_value >= -1.00f + x_value_center) && (history[1] + wind_y_value >= -1.00f + y_value_center && history[1] + wind_y_value <= 1.00f + y_value_center))
                 {
                     sum += 10f;
                 }  
-                else if ((history[0] <= 2.00f && history[0] >= -2.00f) && (history[1] >= -2.00f && history[1] <= 2.00f))
+                else if ((history[0] + wind_x_value <= 2.00f + x_value_center && history[0] + wind_x_value >= -2.00f + x_value_center) && (history[1] + wind_y_value >= -2.00f + y_value_center && history[1] + wind_y_value <= 2.00f + y_value_center))
                 {
                     sum += 5f;
                 }
@@ -34,7 +42,7 @@ namespace task3
                 {
                     if (variant == 0)
                     {
-                        if ((history[0] <= 3.00f && history[0] >= -3.00f) && (history[1] >= -3.00f && history[1] <= 3.00f))
+                        if ((history[0] + wind_x_value <= 3.00f + x_value_center && history[0] + wind_x_value >= -3.00f + x_value_center) && (history[1] + wind_y_value >= -3.00f + y_value_center && history[1] + wind_y_value <= 3.00f + y_value_center))
                         {
                             sum += 1f;
                         }
@@ -48,7 +56,9 @@ namespace task3
                 Console.WriteLine("Write your shot point y. If you have finished shooting, then repeat entering \"I have done\"");
                 y = Console.ReadLine();
             }
+            Console.WriteLine("The center is locating x = {0} and y = {1}", x_value_center, y_value_center);
             Console.WriteLine("Your points are {0}!", sum);
+            Console.WriteLine("Speed of wind is x = {0} and y = {1}", wind_x_value, wind_y_value);
         }
     }
 }
