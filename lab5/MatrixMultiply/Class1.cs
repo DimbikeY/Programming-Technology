589159dac21c7d54cbda27450316d7828a2150b1
DimbikeY <mr.dolmatov-2001@mail.ru>
2021-11-12 20:22:58 +0300

    Well done!

---
+++ lab5/MatrixMultiply/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiply
+{
+    class DataHandling
+    {  
+        public static int[] Enter()
+        {
+            List<int> nums = new List<int>();   // динамический массив
+            string answer = "Start";
+            while (answer != "Stop")
+            {
+                Console.WriteLine("Write down the desired number.");
+                int input = int.Parse(Console.ReadLine());
+                nums.Add(input);
+                Console.WriteLine("Would you like to continue entering some numbers? (Yes). If you get tired, just write \"Stop\".");
+                answer = Console.ReadLine();
+            }
+            int[] new_array = nums.ToArray(); // convert from list to array
+            return new_array;
+        }
+        public static int Summa(int[] new_array)
+        {
+            int summa = 0;
+            foreach (int element in new_array)
+            {
+                summa += element;
+            }
+            Console.WriteLine("Your summa is equal {0}.", summa);
+            return summa;
+        }
+        public static void Average(int[] new_array, int summa)
+        {
+            float average = 0;
+            average = summa / new_array.Length;
+            Console.WriteLine("The average value is {0}.", average);
+        }
+        public static void side_summa_difference(int[] new_array)
+        {
+            int sum = 0;
+            Console.WriteLine("If you want to sum all positive numbers, write \"Plus\". Otherwise, \"Min\".");
+            if (Console.ReadLine() == "Plus")
+            {
+                foreach (int element in new_array)
+                {
+                    if (element > 0)
+                    {
+                        sum += element;
+                    }
+                }
+                Console.WriteLine("Sum of positive numbers is {0}.", sum);
+            }
+            else
+            {
+                foreach (int element in new_array)
+                {
+                    if (element < 0)
+                    {
+                        sum += element;
+                    }
+                }
+                Console.WriteLine("Sum of negative numbers is {0}., sum");
+            }
+        }
+        public static void OddEven(int[] new_array)
+        {
+            int summa = 0;
+            Console.WriteLine("Do you want to find a sum with odd numbers or even? Write \"Even\" or \"Odd\".");
+            if (Console.ReadLine() == "Even")
+            {
+                foreach (int number in new_array)
+                {
+                    if (number % 2 == 0)
+                    {
+                        summa += number;
+                    }
+                }
+                Console.WriteLine("The sum of even numbers is {0}.", summa);
+            }
+            else
+            {
+                foreach (int number in new_array)
+                {
+                    if (number % 2 != 0)
+                    {
+                        summa += number;
+                    }
+                }
+                Console.WriteLine("The sum of odd numbers is {0}.", summa);
+            }
+        }
+        // Доп.задание
+        public static void MaxMin(int[] new_array, ref int min_index, ref int max_index)
+        {
+            string choice = "Nothing";
+            Console.WriteLine("Would you like to find a minimum or maximum number? Type \"Max\" or \"Min\".");
+            int max = new_array.Max();
+            int min = new_array.Min();
+            max_index = Array.IndexOf(new_array, max); // Нашли первое совпадение
+            min_index = Array.IndexOf(new_array, min);
+            if (Console.ReadLine() == "Max")
+            {
+                choice = "Max";
+                Console.WriteLine("{0} of our array is {1}. Index = {2}.", choice, max, max_index);
+            }
+            else
+            {
+                choice = "Min";
+                Console.WriteLine("{0} of our array is {1}. Index = {2}.", choice, min, min_index);
+            }
+        }
+        public static void Multipulti(int[] new_array, int max_index, int min_index)
+        {
+            int mult = 1; // так как произведение
+            for (int num = min_index; num <= max_index; num++)
+            {
+                mult *= new_array[num];
+            }
+            Console.WriteLine("Multiplication between {0} and {1} is {2}.", min_index, max_index, mult);
+        }
+    }
+}
--- lab5/MatrixMultiply/MatrixMultiply.cs
+++ lab5/MatrixMultiply/MatrixMultiply.cs
@@ -6,24 +6,73 @@ namespace MatrixMultiply
     {
         static void Main(string[] args) // Используем рефакторинг с извлечением метода
         {
-            int[,] a = new int[2, 2];
-            NewMethod(a);
-            int[,] b = new int[2, 2];
-            b[0, 0] = 5; b[0, 1] = 6;
-            b[1, 0] = 7; b[1, 1] = 8;
-            int[,] result = new int[2, 2];
-            result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0]; result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
-            result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0]; result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            int min_index = 1;
+            int max_index = 1;
+            int[,] a = Input_a();
+            int[,] b = Input_b();
+            int[,] result = Multiply(a, b); // инициализация массива
             Output(result); // вызов метода
-
+            Console.WriteLine("*************************************************"); // Следующее упражнение 
+            Console.WriteLine("******************THE NEXT TASK******************"); // в виде класса
+            Console.WriteLine("Are you ready to continue? (Yes or No)");
+            string answer = Console.ReadLine();
+            if (answer == "Yes" | answer == "YES")
+            {
+                int[] new_array = DataHandling.Enter();
+                int summa = DataHandling.Summa(new_array);
+                DataHandling.Average(new_array, summa);
+                DataHandling.side_summa_difference(new_array);
+                DataHandling.OddEven(new_array);                              // Не используя объекты класса, поскольку методы static.
+                DataHandling.MaxMin(new_array, ref min_index, ref max_index); // Можно было и со связкой return - out, запихнув два индекса в массив, но скучно!
+                DataHandling.Multipulti(new_array, max_index, min_index);
+            }
+            Console.WriteLine("That's all!");
         }
 
-        private static void NewMethod(int[,] a)
+        private static int[,] Input_b()
         {
-            a[0, 0] = 1; a[0, 1] = 2;
-            a[1, 0] = 3; a[1, 1] = 4;
+            int[,] b = new int[2, 2];
+            for (int r = 0; r < b.GetLength(0); r++)
+            {
+                for (int c = 0; c < b.GetLength(1); c++)
+                {
+                    Console.Write("Enter value for [{0}, {1}] : ", r, c);
+                    string s = System.Console.ReadLine(); // так как внутри другого метода
+                    b[r, c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+            return b;
         }
 
+        private static int[,] Input_a() 
+        {
+            int[,] a = new int[2, 2];
+            for (int r = 0; r < a.GetLength(0); r++) 
+            {
+                for (int c = 0; c < a.GetLength(1); c++)
+                {
+                    Console.Write("Enter value for [{0}, {1}] : ", r, c);
+                    string s = System.Console.ReadLine(); // так как внутри другого метода
+                    a[r, c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+            return a;
+        }
+
+        private static int[,] Multiply(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    result[r, c] = a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+            return result; // поиск переменной по правилу LEGB (сначала result от локала, потом перезапись на глобал)
+        }
         private static void Output(int[,] result) // не выводит ничего, т.к. void
         {
             for (int r = 0; r < result.GetLength(0); r++) // для проверки верхней границы обоих массивов используем вызов метода GetLength
@@ -33,9 +82,7 @@ namespace MatrixMultiply
                     Console.Write("{0} ", result[r, c]);
                 }
                 Console.WriteLine();
-
             }
         }
-
     }
 }
